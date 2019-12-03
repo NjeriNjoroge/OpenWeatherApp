7e82b9b8ec36f147b43d9402951416b16e160828
NjeriNjoroge <grace.njoroge@incentro.com>
2019-12-03 15:44:52 +0300

    initial commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Grace Njoroge on 03/12/2019.
+//  Copyright © 2019 Grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
